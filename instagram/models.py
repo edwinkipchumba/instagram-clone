f47bd8a20a880b687ce1f9f0e691d7bfb867d069
edwinkipchumba <edwin.kolem@student.moringaschool.com>
2021-10-16 21:44:18 +0300

    adding class profile model

--- instagram/models.py
+++ instagram/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    biography = models.TextField(blank=True)
+    phone_number = models.CharField(max_length=20, blank=True)
+    profile_pic = models.ImageField(blank=True, null=True)
+
+    def __str__(self):
+        return self.user.username
+
+    def split_biography(self):
+        return self.biography.split("\n")
