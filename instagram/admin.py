629537a5536c72fcb773ed837e36e1c7c49f4299
edwinkipchumba <edwin.kolem@student.moringaschool.com>
2021-10-16 21:53:38 +0300

    adding admin register sites

--- instagram/admin.py
+++ instagram/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Post,Profile,Comment
+
 
 # Register your models here.
+admin.site.register(Post)
+admin.site.register(Profile)
+admin.site.register(Comment)
