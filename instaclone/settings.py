cb2cd3baee89df7291816b02b918efecb46085a5
edwinkipchumba <edwin.kolem@student.moringaschool.com>
2021-10-17 18:18:02 +0300

    add secret key

--- instaclone/settings.py
+++ instaclone/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8u=bih0$8_h)cwa8cax2c^%09c41ccg19giwgg_!^z@m)9t&fk'
+SECRET_KEY = 'cwa8cax2c^%09c41ccg19giwgg_!^z@m)9t&fk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
