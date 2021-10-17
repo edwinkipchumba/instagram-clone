22167429921a5892fe7af192db6ca2ac8dae31c6
edwinkipchumba <edwin.kolem@student.moringaschool.com>
2021-10-17 18:32:01 +0300

    update settings

--- instaclone/settings.py
+++ instaclone/settings.py
@@ -132,9 +132,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-LOGIN_REDIRECT_URL = 'home'
-LOGOUT_REDIRECT_URL = 'home'
+
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
