29235eb29efd605c9af880b678697573d6790661
edwinkipchumba <edwin.kolem@student.moringaschool.com>
2021-10-17 21:49:26 +0300

    update settings

--- instaclone/asgi.py
+++ instaclone/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'instagram.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'instaclone.settings')
 
 application = get_asgi_application()
--- instaclone/wsgi.py
+++ instaclone/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'instagram.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'instaclone.settings')
 
 application = get_wsgi_application()
