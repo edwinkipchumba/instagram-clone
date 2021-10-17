b40418e46170673b9cb42e7c4a72473179ec97d4
edwinkipchumba <edwin.kolem@student.moringaschool.com>
2021-10-17 09:20:59 +0300

    adding user to followers

--- instagram/views.py
+++ instagram/views.py
@@ -207,3 +207,12 @@ def unfollow(request, to_unfollow):
         unfollow_d = Follow.objects.filter(follower=request.user.profile, followed=user_profile2)
         unfollow_d.delete()
         return redirect('user_profile', user_profile2.user.username)
+    
+    # follow
+
+def follow(request, to_follow):
+    if request.method == 'GET':
+        user_profile3 = Profile.objects.get(pk=to_follow)
+        follow_s = Follow(follower=request.user.profile, followed=user_profile3)
+        follow_s.save()
+        return redirect('user_profile', user_profile3.user.username)
