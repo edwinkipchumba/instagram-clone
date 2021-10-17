b865fa7d50edec052c1ed74d7eb65541d9e19ca4
edwinkipchumba <edwin.kolem@student.moringaschool.com>
2021-10-17 09:20:04 +0300

    adding user to unfollow

--- instagram/views.py
+++ instagram/views.py
@@ -198,3 +198,12 @@ def search_profile(request):
     else:
         message = "You haven't searched for any image category"
     return render(request, 'instagram/results.html', {'message': message})
+
+# unfollow
+
+def unfollow(request, to_unfollow):
+    if request.method == 'GET':
+        user_profile2 = Profile.objects.get(pk=to_unfollow)
+        unfollow_d = Follow.objects.filter(follower=request.user.profile, followed=user_profile2)
+        unfollow_d.delete()
+        return redirect('user_profile', user_profile2.user.username)
