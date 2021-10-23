e54b6035c3e26de34fe20301c6d40c4a5d3b9258
edwinkipchumba <edwin.kolem@student.moringaschool.com>
2021-10-23 20:16:38 +0300

    activate mail receiver

--- instagram/email.py
+++ instagram/email.py
@@ -3,7 +3,7 @@ from django.template.loader import render_to_string
 
 def send_welcome_email(name,receiver):
   subject = 'Welcome to your instagram account'
-  sender = 'shalin.rono@student.moringaschool.com'
+  sender = 'edwin.kolem@student.moringaschool.com'
   text_content = render_to_string('email/welcome.txt',{"name":name})
   html_content = render_to_string('email/welcome.html',{"name":name})
   msg = EmailMultiAlternatives(subject,text_content,sender,[receiver])
